a250885ed154c5bf5693f6c234b3d6949556ede8
j-lambs <jambis187@gmail.com>
2023-09-23 11:40:09 -0700

    made week5 file

--- week 4 - tic tac toe/tic_tac_toe.java
+++ week 4 - tic tac toe/tic_tac_toe.java
@@ -4,16 +4,16 @@ public class tic_tac_toe {
     public static void main(String args[])
     {
         // notes, trying to figure this out
-        /**
-         * each octal digit represents 3 bits in our gamestate
-         * if - the octal digit is a 7 or a 3 (in the first bits 0-8 bits), then if the corresponding
-         * octal digit (3 octal digits to the left (or [9-12] bits)) is also the same number (7 or 3), then either X or O wins, respectively.
-         * else if - (bits 0-8 are all ones with bitwise &), then cats game
-         * else - draw
-         * just didn't have time to do this :c
-         * 
-         */
-        
+
+//         * each octal digit represents 3 bits in our gamestate
+//         * if - the octal digit is a 7 or a 3 (in the first bits 0-8 bits), then if the corresponding
+//         * octal digit (3 octal digits to the left (or [9-12] bits)) is also the same number (7 or 3), then either X or O wins, respectively.
+//         * else if - (bits 0-8 are all ones with bitwise &), then cats game
+//         * else - draw
+//         * just didn't have time to do this :c
+
+
+
         Scanner s = new Scanner(System.in);
 
         int numGames = s.nextInt();
