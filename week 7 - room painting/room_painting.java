5dbdbe8f32e121a92f149596eea8a24503b6fac8
j-lambs <jambis187@gmail.com>
2023-10-07 11:58:03 -0700

    attempt 5 roompaint - delete function call

--- week 7 - room painting/room_painting.java
+++ week 7 - room painting/room_painting.java
@@ -9,15 +9,15 @@ class roomPainting {
      * @param myScanner
      * @return sizesOfferedList, a sorted array of the paint can sizes the store offers
      */
-    private static int[] storeOfferedSizes(int n, Scanner myScanner) {
-        int[] sizesOfferedList = new int[n];
-        for (int i = 0; i < n; ++i) {
-            sizesOfferedList[i] = myScanner.nextInt();
-            myScanner.nextLine();
-        }
-        Arrays.sort(sizesOfferedList);
-        return sizesOfferedList;
-    }
+//    private static int[] storeOfferedSizes(int n, Scanner myScanner) {
+//        int[] sizesOfferedList = new int[n];
+//        for (int i = 0; i < n; ++i) {
+//            sizesOfferedList[i] = myScanner.nextInt();
+//            myScanner.nextLine();
+//        }
+//        Arrays.sort(sizesOfferedList);
+//        return sizesOfferedList;
+//    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -26,9 +26,16 @@ class roomPainting {
         int numSizesNeeded = scanner.nextInt();
         scanner.nextLine();
 
-        int[] sizesOfferedList = storeOfferedSizes(numSizesOffered, scanner);
+        // not calling a function saves O(n) operation? hopefully helps
+        int[] sizesOfferedList = new int[numSizesOffered];
+        for (int i = 0; i < numSizesOffered; ++i) {
+            sizesOfferedList[i] = scanner.nextInt();
+            scanner.nextLine();
+        }
+        Arrays.sort(sizesOfferedList);
 
         // find paint wasted
+        // code segment is O(m * logn)
         int paintWasted = 0;
         for (int i = 0; i < numSizesNeeded; ++i) { // loop through paints Joe is looking for
             int nextSizeNeeded = scanner.nextInt();
@@ -36,13 +43,12 @@ class roomPainting {
 
             int indexAt = Arrays.binarySearch(sizesOfferedList, nextSizeNeeded); // logn
             if (indexAt < 0) {
-                indexAt = -1 * (indexAt+1); // oops didn't read binarySearch documentation
+                indexAt = -1 * (indexAt + 1); // oops didn't read binarySearch documentation
             }
-
             paintWasted += sizesOfferedList[indexAt] - nextSizeNeeded;
         }
 
         scanner.close();
         System.out.println(paintWasted);
-    }
+    } // total big O should be: O(n+logn + m*logn)
 }
